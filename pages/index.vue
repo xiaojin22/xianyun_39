b0f073992874d59ab641f1cee76d851f59965b4a
夏荷 <5511265+xia_he@user.noreply.gitee.com>
2019-12-01 14:22:17 +0800

    05-首页轮播图完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,60 @@
 <template>
-    <div class="index">
-      <span>首页</span>
-    </div>
+  <div class="container">
+    <!-- 幻灯片;;elemnt—ui；轮播图 -->
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="(value, index) in imgLunbo" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                background:url(${$axios.defaults.baseURL}${value.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      //轮播图数据
+      imgLunbo: [
+        // {
+        //   url: "http://157.122.54.189:9095/assets/images/th03.jfif"
+        // },
+        // {
+        //   url: "http://157.122.54.189:9095/assets/images/th04.jfif"
+        // }
+      ]
+    };
+  },
+  //获取轮播数据
+     mounted(){
+        this.$axios({
+            url: "/scenics/banners"
+        }).then(res => {
+           console.log(res)
+           this.imgLunbo=res.data.data
+        })
+    }
+};
 </script>
 
 <style scoped lang="less">
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
-*{
-  margin:0; 
-  padding:0;
-}
-ul, li, ol{
-  list-style:none;
-}
-a{
-  text-decoration:none;
-  color:inherit;
-}
-a:hover{
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
 
-}
-em,i{
-  font-style: normal;
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
