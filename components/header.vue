bcdf1ba74ff8aa9367ea59c886d0ee0d47be597e
夏荷 <5511265+xia_he@user.noreply.gitee.com>
2019-12-03 17:53:05 +0800

    13，用户信息展示1

--- components/header.vue
+++ components/header.vue
@@ -1,63 +1,73 @@
 <template>
-    <header class="header">
-        <el-row type="flex" justify="space-between" class="main">
+  <header class="header">
+    <el-row type="flex" justify="space-between" class="main">
+      <!-- logo -->
+      <div class="logo">
+        <nuxt-link to="/">
+          <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
+        </nuxt-link>
+      </div>
 
-            <!-- logo -->
-            <div class="logo">
-                <nuxt-link to="/">
-                    <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
-                </nuxt-link>
-            </div>
+      <!-- 菜单栏 -->
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">
+          首页
+        </nuxt-link>
+        <nuxt-link to="/post">
+          旅游攻略
+        </nuxt-link>
+        <nuxt-link to="/hotel">
+          酒店
+        </nuxt-link>
+        <nuxt-link to="/air">
+          国内机票
+        </nuxt-link>
+      </el-row>
 
-            <!-- 菜单栏 -->
-            <el-row type="flex" class="navs">
-                <nuxt-link to="/">首页</nuxt-link>
-                <nuxt-link to="/post">旅游攻略</nuxt-link>
-                <nuxt-link to="/hotel">酒店</nuxt-link>
-                <nuxt-link to="/air">国内机票</nuxt-link>  
-            </el-row>
+      <!-- 登录/用户信息 -->
+      <el-row type="flex" align="middle">
+        <!-- 如果用户存在则展示用户信息，用户数据来自store；；根据数据中的token进行判断 -->
+        <el-dropdown v-if="$store.state.user.userInfo.token">
+          <el-row type="flex" align="middle" class="el-dropdown-link">
+            <nuxt-link to="#">
+              <img :src="$axios.defaults.baseURL+$store.state.user.userInfo.user.defaultAvatar">
+              {{ $store.state.user.userInfo.user.nickname }}
+            </nuxt-link>
+            <i class="el-icon-caret-bottom el-icon--right" />
+          </el-row>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <nuxt-link to="#">
+                个人中心
+              </nuxt-link>
+            </el-dropdown-item>
+            <el-dropdown-item>
+              <div @click="handleLogout">
+                退出
+              </div>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
 
-            <!-- 登录/用户信息 -->
-            <el-row type="flex" align="middle">
-               
-                <!-- 如果用户存在则展示用户信息，用户数据来自store；；根据数据中的token进行判断 -->
-                <el-dropdown v-if="$store.state.user.userInfo.token">
-                    <el-row type="flex" align="middle" class="el-dropdown-link">
-                        <nuxt-link to="#">
-                            <img  :src="$axios.defaults.baseURL+$store.state.user.userInfo.defaultAvatar"/>
-                            用户名
-                        </nuxt-link>
-                        <i class="el-icon-caret-bottom el-icon--right"></i>
-                    </el-row>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>
-                           <nuxt-link to="#">个人中心</nuxt-link>
-                        </el-dropdown-item>
-                        <el-dropdown-item>
-                            <div @click="handleLogout">退出</div> 
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-
-                <!-- 不存在用户信息展示登录注册链接 -->
-                <nuxt-link to="/user/login" class="account-link" v-else>
-                    登录 / 注册 
-                </nuxt-link>
-            </el-row>
-        </el-row>
-    </header>
+        <!-- 不存在用户信息展示登录注册链接 -->
+        <nuxt-link v-else to="/user/login" class="account-link">
+          登录 / 注册
+        </nuxt-link>
+      </el-row>
+    </el-row>
+  </header>
 </template>
 
 <script>
 export default {
-    methods: {
-        // 用户退出
-        handleLogout(){},
-    },
-    mounted(){
-        console.log(1234)
-        console.log(this.$store.state.user.userInfo.token)
-    }
+  mounted () {
+    console.log(1234)
+    console.log(this.$store.state.user.userInfo.token)
+  },
+  methods: {
+    // 用户退出
+    handleLogout () {}
+  }
 }
 </script>
 
@@ -113,7 +123,7 @@ export default {
             cursor:pointer;
             .el-icon-bell{
                 margin-right:2px;
-                font-size:18px;      
+                font-size:18px;
             }
         }
 
@@ -150,5 +160,5 @@ export default {
                 text-decoration: underline;
             }
         }
-    } 
+    }
 </style>
