75b04c919652070d4f013642969f522c824f153c
夏荷 <5511265+xia_he@user.noreply.gitee.com>
2019-12-05 14:46:11 +0800

    35-机票列表引入航班头部布局组件

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="flights">
-    机票页面
-  </div>
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 其他代码.. -->
+
+        <!-- 航班头部布局；使用子组件 -->
+        <FlightsHeaderList />
+
+        <!--  其他代码... -->
+      </div>
+
+      <!--  其他代码... -->
+    </el-row>
+  </section>
 </template>
 
 <script>
+// 引入子组件
+import FlightsHeaderList from '@/components/air/flightsHeaderList.vue'
+
 export default {
+  // 注册子组件
+  components: {
+    FlightsHeaderList
+  }
 }
 </script>
 
 <style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
 
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    }
 </style>
