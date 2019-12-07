e1b62cfcd58d01d746ca08bdf22e43a9e8501354
夏荷 <5511265+xia_he@user.noreply.gitee.com>
2019-12-07 22:57:30 +0800

    实现撤销筛选过滤器的数据；撤销功能

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -171,7 +171,16 @@ export default {
     },
 
     // 撤销条件时候触发
-    handleFiltersCancel () {}
+    handleFiltersCancel () {
+      // 将所有筛选的数据进行清空
+      this.company = ''
+      this.flightTimes = ''
+      this.airSize = ''
+      this.airport = ''
+      // 先清空过滤器条件,然后子传父传回原始数据 比较笨不容易理解的方法
+      // 直接执行runFilter 更加简单；就是将清空后的数据传递会父组件
+      this.runFlights()
+    }
   }
 }
 </script>
