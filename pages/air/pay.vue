40b275d0de82d8c5aeaab08cf8bf9e524f9a9234
夏荷 <5511265+xia_he@user.noreply.gitee.com>
2019-12-10 01:05:50 +0800

    支付页面的渲染价格和wath监听token，下载二维码包

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="main">
       <div class="pay-title">
-        支付总金额 <span class="pay-price">￥ 1000</span>
+        支付总金额 <span class="pay-price">￥ {{ allData.price }}</span>
       </div>
       <div class="pay-main">
         <h4>微信支付</h4>
@@ -29,7 +29,45 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 订单详情
+      allData: {}
+    }
+  },
+  // 页面一刷新，就没有token值，所以要监听token的变化
+  watch: {
+    'this.$store.state.user.userInfo.token' () {
+      // 封装页面一刷新就报没有token的错误
+      this.loadData()
+    }
+  },
+  // 获取订单详情。渲染页面价格
+  mounted () {
+    // 判断有无token值
+    if (this.$store.state.user.userInfo.token) {
+      this.loadData()
+    }
+  },
+  methods: {
+    // 封装获取订单详情的方法
+    loadData () {
+      // 获取token值this.$store.state.user.userInfo.token
+      const token = this.$store.state.user.userInfo.token// 获取vuex中的token值
+      // 判断有无token值，再发送axios请求，获取订单详情
+      if (token) {
+        this.$axios({
+          url: '/airorders/' + this.$route.query.id,
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        }).then((res) => {
+          window.console.log(res.data)
+          this.allData = res.data// 订单详情
+        })
+      }
+    }
+  }
 }
 </script>
 
